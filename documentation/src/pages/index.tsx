632c960a27367db5d1e3ac9477f463646e649823
martinvibes <martinmachiebe21@gmail>
2026-02-22 17:34:55 +0100

    updates

--- documentation/src/pages/index.tsx
+++ documentation/src/pages/index.tsx
@@ -19,12 +19,12 @@ function HomepageHeader() {
         </Heading>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
-          <Link
+          <a
             className="button button--secondary button--lg"
-            to="/getting-started/setup"
+            href="/getting-started/setup"
           >
             Start Building with Soroban
-          </Link>
+          </a>
         </div>
       </div>
     </header>
